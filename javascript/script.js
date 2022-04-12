d84992a0881820373bd729371e676087e48af410
Ennio-M <marrese.ennio@gmail.com>
2022-04-12 02:25:23 +0200

    milestone 3

--- javascript/script.js
+++ javascript/script.js
@@ -113,22 +113,43 @@ const icons = [
 	}
 ];
 
+// Variabili e impostazione iniziale pagina
 const app = document.getElementById("app");
 const row = document.createElement("div");
 row.classList.add("row", "row-cols-5", "g-5");
 app.append(row);
-icons.forEach((element, i) => {
-    const col = document.createElement("div");
-    col.setAttribute("class", "col")
-    const card = document.createElement("div");
-    card.setAttribute("class", "icon_container");
-    card.style.color = `${element.color}`
-    const icon = document.createElement("i");
-    icon.setAttribute("class", `${element.family} ${element.prefix}${element.name}`);
-    const h6 = document.createElement("h6");
-    h6.innerHTML = `${element.name.toUpperCase()}`
-    card.append(icon);
-    card.append(h6);
-    col.append(card);
-    row.append(col);
-});
+printPage(icons);
+
+// Evento
+const type = document.getElementById("type");
+type.addEventListener("change", filterBy);
+
+// Funzioni
+// Stampa la pagina una card alla volta
+function printPage(arr) {
+    arr.forEach((element) => {
+        const col = document.createElement("div");
+        col.setAttribute("class", "col")
+        const card = document.createElement("div");
+        card.setAttribute("class", "icon_container");
+        card.style.color = `${element.color}`
+        const icon = document.createElement("i");
+        icon.setAttribute("class", `${element.family} ${element.prefix}${element.name}`);
+        const h6 = document.createElement("h6");
+        h6.innerHTML = `${element.name.toUpperCase()}`
+        card.append(icon);
+        card.append(h6);
+        col.append(card);
+        row.append(col);
+    });
+}
+ // Crea l'array filtrato e lo stamoa
+function filterBy() {
+    const filtered = icons.filter(checkType);
+    row.innerHTML = "";
+    printPage(filtered);
+}
+// Funzione per filtrare l'array
+function checkType(element) {
+    return element.type == type.value || type.value == "all";
+}
